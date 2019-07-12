85725492f42084391d83db43b19ef68998125fba
phankerson4 <phankerson4@gmail.com>
2019-07-11 23:25:44 -0400

    Added Empty Array and Search Variable

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
+let allArtist = []
+let searchAPI= ''
 let button = document.querySelector('#submit')
 let input = document.querySelector('#search')
 let output = document.querySelector('#output')
-
-
 
 button.addEventListener('click', function () {
     fetch('https://itunes-api-proxy.glitch.me/search?term='+input.value)
       .then(function (response) {
         return response.json()
       })
-      .then(function (data) {
-        console.log(data)
+      .then(function(data){
+          console.log(data)
       })
-})
-
+    })

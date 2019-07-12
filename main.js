99646dfb813f357f98506acc402334505067b0b6
phankerson4 <phankerson4@gmail.com>
2019-07-12 14:20:31 -0400

    Added Cards & Audio Links

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-let allArtist = []
-let searchAPI= ''
+
 let button = document.querySelector('#submit')
 let input = document.querySelector('#search')
 let output = document.querySelector('#output')
 
-button.addEventListener('click', function () {
+button.addEventListener('click', (e) =>{
     fetch('https://itunes-api-proxy.glitch.me/search?term='+input.value)
-      .then(function (response) {
-        return response.json()
+    .then(data => data.json())
+    .then(json => {
+        console.log(json)
+        let searchAPI= ''
+
+     
+
+          json.results.forEach(song => {
+            searchAPI += `
+             <div class = "col s4 m4 l4>
+            <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="${song.artworkUrl100}">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right">more_vert</i></span>
+              <p>${song.artistname}</p>
+            </div>
+            <div class="play-song">
+            <audio controls src="${song.previewUrl}"></audio>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+            </div>
+            </div>
+         
+         
+               `
+               
+          })
+
+          
+          output.innerHTML = searchAPI
+        })
+        
+        .catch(error => console.log(error))
       })
-      .then(function(data){
-          console.log(data)
-      })
-    })
+    
+    
+    
+    

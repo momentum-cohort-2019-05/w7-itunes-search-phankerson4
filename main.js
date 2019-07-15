42c23adf8642613a322573e9f0ab17a6530e508f
phankerson4 <phankerson4@gmail.com>
2019-07-15 09:39:28 -0400

    Completed Search Website

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-
 let button = document.querySelector('#submit')
 let input = document.querySelector('#search')
 let output = document.querySelector('#output')
+
+
+
 
 button.addEventListener('click', (e) =>{
     fetch('https://itunes-api-proxy.glitch.me/search?term='+input.value)
@@ -17,20 +19,28 @@ button.addEventListener('click', (e) =>{
              <div class = "col s4 m4 l4>
             <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
-              <img src="${song.artworkUrl100}">
+              <img class="activator" src="${song.artworkUrl100}">
+              
             </div>
+           
             <div class="card-content">
               <span class="card-title activator grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right">more_vert</i></span>
               <p>${song.artistname}</p>
+              
+              
+              
             </div>
             <div class="play-song">
             <audio controls src="${song.previewUrl}"></audio>
             <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+              <span class= "card-title grey-text text-darken-4">${song.collectionCensoredName}<i class="material-icons right">close</i></span>
+              <p>Genre:${song.primaryGenreName}</p>
+              <p></p>
+              
             </div>
             </div>
             </div>
+            
          
          
                `
